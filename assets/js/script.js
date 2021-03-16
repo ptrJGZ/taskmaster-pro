3a3bce119c5c97bf74bde1867eaf5f83c528fb6a
Peter J. Gonzalez <ptr.jgz@gmail.com>
2021-03-15 23:53:22 -0400

    Add datepicker and audit dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -88,7 +90,7 @@ $(".list-group").on("blur", "textarea", function () {
   $(this).replaceWith(taskP);
 });
 
-// Note: event listener for click on date
+// Note: event listener for click on date. Editing Date feature
 // due date was clicked
 $(".list-group").on("click", "span", function () {
   // get current date
@@ -105,6 +107,15 @@ $(".list-group").on("click", "span", function () {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1, // minimum date = 1 day after current day
+    onClose: function () {
+      // when calendar is closed, force a "change" event on the `dateInput`
+      $(this).trigger("change"); // Fixes blur problem
+    }
+  });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 });
@@ -112,7 +123,8 @@ $(".list-group").on("click", "span", function () {
 // Note: event lister for clicking out of date
 // value of due date was changed
 // Note: blur means when element loses focus
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
+  //$(".list-group").on("blur", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
@@ -140,6 +152,9 @@ $(".list-group").on("blur", "input[type='text']", function () {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // Note: this = the list currently being affected
@@ -244,6 +259,31 @@ $("#task-form-modal .btn-primary").click(function () {
     saveTasks();
   }
 });
+
+// Date picker
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+var auditTask = function (taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } // check if date is imminent
+  else if (Math.abs(moment().diff(time, "days")) <= 3) { // reads left to right and returns -2, so absolute value to get 2
+    $(taskEl).addClass("list-group-item-warning");
+  }
+
+};
 
 // remove all tasks
 $("#remove-tasks").on("click", function () {
